b6c68d7b37a2a6de62716acb95175e9552edb5ae
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-12-24 09:23:48 -0700

    Revert to previous commit & fix model

--- models/index.js
+++ models/index.js
@@ -20,6 +20,11 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id'
 });
 
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
 module.exports = {
   Product,
   Category,
